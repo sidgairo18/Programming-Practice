31d2becde4628121741fbe924c101439bc8ca5b9
sidgairo18 <siddhartha.gairola18@gmail.com>
2018-11-21 15:09:36 +0530

    Heap begins

---
+++ practice/heap/priority_queue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    priority_queue<int> max_heap;
+    priority_queue< int, vector<int>, greater<int> > min_heap;
+
+    return 0;
+}

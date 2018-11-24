982053e4feea3ef2e7a79c3c9ebbad7767bf8878
sidgairo18 <siddhartha.gairola18@gmail.com>
2018-11-24 14:07:07 +0530

    new day

---
+++ practice/dp_n_strings/count_stairs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int n;
+        cin>>n;
+
+        long long int dp[n+1];
+        dp[0] = dp[1] = 1;
+
+        for(int i = 2; i<=n; i++)
+            dp[i] = dp[i-1]+dp[i-2];
+
+        cout<<dp[n]<<endl;
+
+    }
+
+    return 0;
+}
